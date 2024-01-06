de69ac8e5e696dacc79d7cf134ab04f837f880f6
ahmet <codingwithmish@gmail.com>
2024-01-06 20:09:53 +0300

    added footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,89 @@
 import * as S from './styled'
 
+import { Link } from 'react-router-dom'
+
+import InstagramIcon from '../../assets/instagram-icon.svg'
+import FacebookIcon from '../../assets/facebook-icon.svg'
+import LinkedInIcon from '../../assets/linkedin-icon.svg'
+import TwitterIcon from '../../assets/twitter-icon.svg'
+
 export default function Footer() {
   return (
     <S.Footer>
-        
+        <div className="container">
+          <div className="footer">
+            <div className="categories">
+              <span>Categories</span>
+              <Link>Graphics & Design</Link>
+              <Link>Programming & Tech</Link>
+              <Link>Digital Marketing</Link>
+              <Link>Video & Animation</Link>
+              <Link>Writing & Translation</Link>
+              <Link>Music & Audio</Link>
+              <Link>Business</Link>
+              <Link>Data</Link>
+              <Link>Photography</Link>
+              <Link>AI Services</Link>
+            </div>
+            <div className="about">
+              <span>About</span>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+            </div>
+            <div className="supportandeducation">
+              <span>Support and Education</span>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+            </div>
+            <div className="community">
+              <span>Community</span>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+            </div>
+            <div className="business">
+              <span>Business Solutions</span>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+              <Link>( Item )</Link>
+            </div>
+          </div>
+          <div className="sub-footer">
+            <div className="left">
+              <h2>fiverr.</h2>
+              <span>© Fiverr International Ltd. 2024</span>
+            </div>
+            <div className="right">
+              <img src={InstagramIcon} />
+              <img src={FacebookIcon} />
+              <img src={LinkedInIcon} />
+              <img src={TwitterIcon} />
+              <span>English</span>
+            </div>
+          </div>
+        </div>
     </S.Footer>
   )
 }
--- src/components/Footer/styled.jsx
+++ src/components/Footer/styled.jsx
@@ -1,5 +1,65 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-    
+    color: #505050;
+    border-top: 1px solid #ebebeb;
+
+    .container {
+        .footer {
+            gap: 16px;
+            opacity: 0.9;
+            display: flex;
+            padding: 16px 0;
+            justify-content: space-between;
+
+            & > * {
+                gap: 6px;
+                display: flex;
+                flex-direction: column;
+
+                span {
+                    font-size: 18px;
+                    font-weight: 600;
+                }
+            }
+        }
+
+        .sub-footer {
+            display: flex;
+            padding: 16px 0;
+            align-items: center;
+            justify-content: space-between;
+            border-top: 1px solid #ebebeb;
+
+            .left {
+                gap: 16px;
+                display: flex;
+                align-items: center;
+
+                h2 {
+                    opacity: 0.9;
+                }
+
+                span {
+                    font-size: 14px;
+                }
+            }
+
+            .right {
+                gap: 8px;
+                display: flex;
+
+                img {
+                    width: 22px;
+                    height: 22px;
+                }
+
+                span {
+                    font-size: 15px;
+                    font-weight: 500;
+                    padding-left: 16px;
+                }
+            }
+        }
+    }
 `
